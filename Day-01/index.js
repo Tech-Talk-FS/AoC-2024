c8c2b9c99fcaf5c12b7aa36f793bf06e5d5e7213
Brad Beltowski <bradbeltowskigmail.com>
2024-12-07 00:58:28 -0500

    Solving Part 2

--- Day-01/index.js
+++ Day-01/index.js
@@ -1,23 +1,36 @@
+// Calculate a total similarity score by adding up each number in the left list 
+// after multiplying it by the number of times that number appears in the right list.
+
 // split string and put the first in left array and the second in right array. 
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, 'puzzle.txt');
+const filePath = path.join(__dirname, 'puzzle1.txt');
+const data = []
+const similarity = (leftArr,rightArr) => {
+      const duplicates = {};
+      for (const item1 of leftArr) {
+        console.log("start");
+        
+        for (const item2 of rightArr) {
+          
+          // console.log("boom");
 
-const sortArrays = (leftArr,rightArr) => {
-    const leftAsc = leftArr.sort((a, b) => a - b)
-    const rightAsc = rightArr.sort((a, b) => a - b)
-
-    let total = 0 
-    for (let i = 0; i < leftAsc.length; i++) {
-        const left = leftAsc[i];
-        const right = rightAsc[i];
-        // makes the negative numbers into a positive distance
-        const distance = Math.abs(left - right)
-        // sets the total distance
-        total += distance
-    }
-    console.log("total", total);
+          if (item1 === item2) {
+            console.log("1",item1);
+            console.log("2",item2);
+            // counts[num] = (counts[num] || 0) + 1;
+            duplicates[item1] += item2
+            // console.log(">>>",duplicates);
+            
+            // duplicates.push(item1);
+            break; // Move to the next element in arr1
+          }
+        }
+      }
+      return duplicates;
 }
+
+
 
 try {
   // Read the file and grab the numbers to work with
@@ -37,7 +50,10 @@ try {
     return parseInt(numbers[1]);
   });
 
-  sortArrays(left,right)
+  const print = similarity(left,right)
+  console.log(print.length);
+  
+
 } catch (err) {
   console.error('Error reading the file:', err);
 }
---
+++ Day-01/part1.js
@@ -0,0 +1,43 @@
+// split string and put the first in left array and the second in right array. 
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'puzzle1.txt');
+
+const sortArrays = (leftArr,rightArr) => {
+    const leftAsc = leftArr.sort((a, b) => a - b)
+    const rightAsc = rightArr.sort((a, b) => a - b)
+
+    let total = 0 
+    for (let i = 0; i < leftAsc.length; i++) {
+        const left = leftAsc[i];
+        const right = rightAsc[i];
+        // makes the negative numbers into a positive distance
+        const distance = Math.abs(left - right)
+        // sets the total distance
+        total += distance
+    }
+    console.log("total", total);
+}
+
+try {
+  // Read the file and grab the numbers to work with
+  const data = fs.readFileSync(filePath, 'utf8');
+
+  // Grabs puzzle.txt and splits them on new line
+  const lines = data.trim().split('\n');
+
+  // Make the left and right arrays
+  const left = lines.map(line => {
+    const numbers = line.split(' ');
+    return parseInt(numbers[0]);
+  });
+
+  const right = lines.map(line => {
+    const numbers = line.split(' ');
+    return parseInt(numbers[1]);
+  });
+
+  sortArrays(left,right)
+} catch (err) {
+  console.error('Error reading the file:', err);
+}
